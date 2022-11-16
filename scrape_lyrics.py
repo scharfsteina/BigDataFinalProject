70271dce8c4e9c1bfeb2c6c12f6a3e2e3a8cd91d
scharfsteina <ava.l.scharfstein.24@dartmouth.edu>
2022-11-16 21:05:31 +0100

    scraped midnight lyrics

--- scrape_lyrics.py
+++ scrape_lyrics.py
@@ -1,25 +1,31 @@
 # Importing Packages
-import requests
-import re
-from bs4 import BeautifulSoup
+import lyricsgenius
+import json, re, os
 
-# scrape list of songs LATER
-#midnights = 'http://api.genius.com/albums/Taylor-swift/Midnights-3am-edition'
-#page = requests.get("https://genius.com/albums/Taylor-swift/Midnights-3am-edition")
-#soup = BeautifulSoup(page.content, "html.parser")
-#print(soup.body.routable-page)
+# Get Midnights from Spotify
+genius = lyricsgenius.Genius('Bearer FjQostjLTcPJ4d4DZI6_ms4KrhA_wovfHJ2siG7rl1cUbHQ_2_Xl__4BGk_3-2Kv')
+album = genius.search_album("Midnights (3am Edition)", "Taylor Swift")
+# Save album as a json file
+album.save_lyrics()
+# Get the saved json file
+album_json = json.load(open('Lyrics_Midnights3amEdition.json'))
 
-midnights_songs = ["lavenderhaze", "maroon", "antihero", "snowonthebeach", "youreonyourownkid", "midnightrain", "question", "vigilanteshit", "bejeweled", "labyrinth", "karma", "sweetnothing", "mastermind", "thegreatwar", "paris", "highinfidelity", "glitch", "wouldvecouldveshouldve", "dearreader"]
-
-# track_n = 0
-# for song in midnights_songs:
-#     url = f"https://www.azlyrics.com/lyrics/taylorswift/{song}.html"
-#     soup = BeautifulSoup(requests.get(url).text, "html.parser").find('div', attrs={'class':'ringtone'})
-#     song_name = soup.find_next('b').contents[0].strip()
-#     lyrics = soup.find_next('div').text.strip()
-#     with open("data/taylorswift_lyrics/10-midnights_3am_version.csv", "w") as w:
-#         for line in lyrics.splitlines():
-#             print(line + "hello")
-#     w.close()
-#
-#     track_n += 1
+# Create a file to write midnight lyrics
+midnights = open('data/taylorswift_lyrics/10-midnights_3am_edition','w')
+midnights.write('album_name,track_title,track_n,lyric,line\n') # column titles ??
+# Iterate through each track
+for i in range(len(album_json['tracks'])):
+    # Get title and lyrics
+    title = album_json['tracks'][i]['song']['title']
+    lyrics = album_json['tracks'][i]['song']['lyrics']
+    # Remove things like [Chorus] and [Verse 1] etc.
+    lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
+    # Remove all of the text before "Track Title Lyrics"
+    lyrics = re.sub(f".*(?={title} Lyrics)", '', lyrics)
+    # Remove the #Embed at the end of the string
+    lyrics = re.sub("\d+Embed$", "", lyrics)
+    # Turns the string into a list containing each line of the song
+    lyrics = [s.strip() for s in lyrics.split('\n') if s]
+    for line in range(len(lyrics)):
+        # Write album name, track title, track number, lyric, and line to midnights file
+        midnights.write("Midnights,%s,%i,%s,%i\n" % (title,i,lyrics[line],line+1))
--- testing.py
+++
@@ -1,8 +0,0 @@
-
-import re, requests
-from bs4 import BeautifulSoup
-
-URL = 'https://genius.com/Andy-shauf-the-magician-lyrics'
-page = requests.get(URL)
-html = BeautifulSoup(page.text, "html.parser")
-lyrics = html.find('div[class^="SongPage__Section"]').get_text().encode('ascii','ignore')
